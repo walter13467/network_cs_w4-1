3203b5e77b4d2d340f55c8a464f02a32b6b597a3
冠德 江 <walter13467@outlook.com>
2022-03-21 11:21:07 +0800

    加入專案檔案。

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace network_cs_w4_1
+{
+    internal class Car
+    {
+        string color = "red";
+        public void ShowInfo()
+        {
+            Console.WriteLine("顏色 " + color);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using network_cs_w4_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0,i = 0;
+
+            Random randomNum = new Random();
+            randomNum.Next(0, 100);
+            int currentNum = randomNum.Next(0, 100);
+
+            Console.WriteLine(currentNum);
+            Console.WriteLine("請輸入0~100的數字");
+            while(ans == currentNum)
+            {
+                
+                ans = Convert.ToInt32(Console.ReadLine());
+                i++;
+                if (i >= 5)
+                {
+                    break;
+                }
+                Console.WriteLine("答錯了 還有"+(5-i)+"次機會");
+            }
+            
+
+        }
+    }
+}
